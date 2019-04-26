6cca79085074144b108adc35f699182d5baa3ef8
Maksatus <maksim040@mail.ru>
2019-04-26 16:02:02 +0400

    add

--- integral/main.cpp
+++ integral/main.cpp
@@ -3,73 +3,75 @@
 # include <locale.h>
 using namespace std;
 
-
 double func(const double &x)
 {
-	return (cos(x));
+	return (2 * x*x*x - 7 * x + 4);
 }
 
 double integr_lefttriangle(const int &n, const double &a, const double &b)
 {
 	double sum = 0;
-	double step = (b - a) / n;
-	for (double i = a; i < b; i = i + step)
-		sum += func(i);
-	return sum * step;
+	double h = (b - a) / n;
+	for (double i = 0; i < n; i++)
+		sum += func(a + i * h);
+	return sum * h;
 }
 double integr_righttriangle(const int &n, const double &a, const double &b)
 {
-	
 	double sum = 0;
-	double step = (b - a) / n;
-	for (double i = 1; i <= n - 1; i++)
-		sum += func(a + i * step);
-	return step * sum;
+	double h = (b - a) / n;
+	double x = a + h; 
+	
+	for (int i = 0; i < n; i++, x += h) 
+	{
+		sum += func(x);
+	}
+	return h * sum;
 }
 double integr_middle(const int &n, const double &a, const double &b)
 {
 	double sum = (func(a) + func(b)) / 2;
-	double step = (b - a) / n;
+	double h = (b - a) / n;
 	for (int i = 1; i <= n - 1; i++)
 	{
-		sum += func(a + i * step);
+		sum += func(a + i * h);
 	}
-	return step * sum;
+	return h * sum;
 }
 
 double I(double a, double b, int n, double y) { return ((b - a) / (2 * n)*y); }
 double trapeze(const int &n, const double &a, const double &b)
 {
 	
-	    double step, sum, x;
+	    double h, sum, x;
 		sum = 0;
-		step = (b - a) / n;
+		h = (b - a) / n;
 		sum += func(a);
 		for (int i = 1; i < n; i++)
 		{
-			x = a + i * step;
+			x = a + i * h;
 			sum += 2 * func(x);
 		}
-		x = a + n * step;
+		x = a + n * h;
 		sum += func(x);
-		return(0.5*step*sum);
+		return(0.5*h*sum);
 }
 double simpson(const int &n, const double &a, const double &b)
 {
-	double step = (b - a) / n;
+	double h = (b - a) / n;
 	double sum = 0;
 
 	double x0 = a;
-	double x1 = a + step;
+	double x1 = a + h;
 
-	for (unsigned i = 0; i <= n - 1; i++) {
-		sum += func(x0) + 4 * func(x0 + step / 2) + func(x1);
+	for (int i = 0; i <= n - 1; i++) {
+		sum += func(x0) + 4 * func(x0 + h / 2) + func(x1);
 
-		x0 += step;
-		x1 += step;
+		x0 += h;
+		x1 += h;
 	}
 
-	return (step / 6)*sum;
+	return (h / 6)*sum;
 
 }
 
