b94d2ee851e988df4efa877d832b1c0594ba564a
Max <45514929+Maksatus@users.noreply.github.com>
2019-12-24 19:09:10 +0400

    Update Source.cpp

--- Stl/Source.cpp
+++ Stl/Source.cpp
@@ -11,16 +11,16 @@ void print(vector<CoordInt>& ob)
 	}
 }
 
-vector<CoordInt> read(vector<CoordInt>& ob,size_t N)
+vector<CoordInt> read(vector<CoordInt>& ob, size_t N)
 {
 	int x = 0, y = 0;
-	for (size_t i=0;i<N;i++)
+	for (size_t i = 0; i < N; i++)
 	{
-		x = rand() % LIMIT +(-25); // -25>>24
+		x = rand() % LIMIT + (-25); // -25>>24
 		y = rand() % LIMIT + (-25);
 		ob.push_back({ x,y });
 	}
-	
+
 	return ob;
 }
 
@@ -28,62 +28,47 @@ void example_1(vector<CoordInt>& matrix)
 {
 	cout << "\nSize: " << matrix.size();
 
-	if (!matrix.empty()) 
-	{ 
-		cout << "\nNot empty\n"; 
-		
+	if (!matrix.empty())
+	{
+		cout << "\nNot empty\n";
+
 		CoordInt front_num = matrix.front();
 		CoordInt back_num = matrix.back();
 		cout << "\nfront_num:" << front_num;
 		cout << "\nback_num:" << back_num;
 		matrix.pop_back();
-			cout << "delete last value\n";
-			print(matrix);
-			//matrix.clear();
+		cout << "delete last value\n";
+		print(matrix);
+		//matrix.clear();
 	}
-	else{ cout << "\nEmpty\n"; }
+	else { cout << "\nEmpty\n"; }
 }
 
-void example_2(vector<CoordInt>& ob,CoordInt val,int rad)
+void example_2(vector<CoordInt>& new_matr, CoordInt val)
 {
-	vector<CoordInt> new_matr;
+	double r = 0;
+	double min = 32999;
+	CoordInt val2;
 
-	int s = 0;
-	for (auto iter = ob.begin(); iter != ob.end(); ++iter)
-	{
-	s = (iter->get_y() - val.get_y())*(iter->get_y() - val.get_y()) + (iter->get_x() - val.get_x())*(iter->get_x() - val.get_x());
-		
-		if (s<= rad*rad)
-		{
-			new_matr.push_back(*iter);
-		}
-	}
-	print(new_matr);
-	
-	CoordInt min = new_matr.front();
 	for (auto iter = new_matr.begin(); iter != new_matr.end(); ++iter)
 	{
-		if (min < (*iter))
+		r = sqrt(iter->get_x() - val.get_x()) * (iter->get_x() - val.get_x()) + (iter->get_y() - val.get_y()) * (iter->get_y() - val.get_y());
+		if (r<min)
 		{
-			min = (*iter);
+			min =r;
+			val2 = (*iter);
 		}
 	}
-	cout << "\nMin= " << min<<endl;
+	cout << "\nMin= " << val2 << endl;
 }
 
 void example_3(vector<CoordInt>& ob)
 {
-	sort(ob.begin(), ob.end(), CoordInt::compare());
-  //sort(ob.begin(), ob.end(), [](CoordInt v1, CoordInt v2) {return v1 < v2; });
+	sort(ob.begin(), ob.end());
 	cout << "\nWork out sort\n";
 	print(ob);
 }
 
-
-void example_4(vector<CoordInt>& ob)
-{	
-	
-}
 
 int main(int argc, char* argv[])
 {
@@ -91,17 +76,14 @@ int main(int argc, char* argv[])
 	vector<CoordInt> matrix;
 	matrix = read(matrix, N);
 	print(matrix);
-	/*example_1(matrix);
+	example_1(matrix);
 	
 	cout << "Enter a point\n";
 	CoordInt val;
 	int radius;
 	cin >> val;
-	cout << "The radius of the circle\n";
-	cin >> radius;
-	example_2(matrix,val,radius);
+	example_2(matrix,val);
 
-	example_3(matrix);*/
-	example_4(matrix);
+	example_3(matrix);
 	return 0;
 }
